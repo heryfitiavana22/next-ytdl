3eb93505137ae758141a89705832dc086b66c0eb
heryfitiavana22 <hheryfitiavana@gmail.com>
2024-12-12 21:59:42 +0300

    update: api removing file

--- app/api/remove/route.ts
+++ app/api/remove/route.ts
@@ -5,14 +5,9 @@ import { removeIfExists } from "@/lib/file-manager";
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const filenameParams = url.searchParams.get("filename");
-  const mediaTypeParams = url.searchParams.get("type");
 
   if (!filenameParams)
     return new Response("Missing filename parameter", { status: 400 });
-
-  if (mediaTypeParams !== "mp4" && mediaTypeParams !== "mp3") {
-    return new Response("Media type not supported", { status: 400 });
-  }
 
   const tempFilename = decodeURIComponent(filenameParams);
   const filename = normalizeFilename(tempFilename);

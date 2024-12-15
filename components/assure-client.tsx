1470e1adbc9529d263fa007be64053962cc27b7f
heryfitiavana22 <hheryfitiavana@gmail.com>
2024-12-15 11:46:09 +0300

    fix: linter error in assure-client

--- components/assure-client.tsx
+++ components/assure-client.tsx
@@ -3,16 +3,18 @@
 import { useIsClient } from "@/hooks/use-is-client";
 import React from "react";
 
-type ComponentType<T extends Object> = (props: T) => React.JSX.Element;
+type ComponentType<T extends object> = (props: T) => React.JSX.Element;
 
-export function assureClient<T extends Object>(
+export function assureClient<T extends object>(
   WrappedComponent: ComponentType<T>
 ) {
-  return (props: T) => {
+  const Component = (props: T) => {
     const isClient = useIsClient();
 
     if (isClient) return <WrappedComponent {...props} />;
 
     return <></>;
   };
+  Component.displayName = `AssureClient(${WrappedComponent.name})`;
+  return Component;
 }

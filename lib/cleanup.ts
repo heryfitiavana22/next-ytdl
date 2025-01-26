5c4f11a53440844d7bb8e25205559111d9c0ce26
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-01-26 09:12:28 +0300

    fix: correct import path for DOWNLOAD_DIR in cleanup module

--- lib/cleanup.ts
+++ lib/cleanup.ts
@@ -2,7 +2,7 @@ import { readdir, stat } from "fs/promises";
 import { join } from "path";
 import { tmpdir } from "os";
 import { removeIfExists } from "./file-manager";
-import { DOWNLOAD_DIR } from "@/features/constants";
+import { DOWNLOAD_DIR } from "../features/constants";
 
 const ONE_HOUR_IN_MS = 3600000;
 let running = false;

8092bd9776f85dde20afc2df29d1c25397798735
Hery Fitiavana <hheryfitiavana@gmail.com>
2025-10-29 09:34:28 +0300

    chore: using next 16.0.1

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
-import { cleanupTempFiles } from "./lib/cleanup";
 
-if (typeof process !== "undefined") {
-  cleanupTempFiles();
-}
 const nextConfig: NextConfig = {
   /* config options here */
 };
